60ccf0d04ff9f1c6ad30152f56fef84968bfffea
Monica Delgado <mnca.delgado@gmail.com>
2022-08-08 08:54:20 -1100

    added firebase folder with firebase config file

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,27 @@
+import firebase from "firebase"
+
+//Setting up web app's configuration
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: "${config.measurementId}",
+}
+
+//initialize firebase with try catch method
+try {
+  console.log("FB", firebaseConfig)
+  firebase.initializeApp(firebaseConfig)
+} catch (err) {
+  if (!/already exists/.test(err.message)) {
+    console.error("Firebase initialization error", err.stack)
+  }
+}
+
+const fireConfig = firebaseConfig
+
+export default fireConfig
