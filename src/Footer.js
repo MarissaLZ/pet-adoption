dd0f57c9cdfb788001a28ce71539fb9594cc9f33
Jayzar <jayzar.lynn@gmail.com>
2022-08-10 11:00:08 -0400

    Adjusted formating of Footer and FooterItems to display in columns.

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { Container, Grid, Box } from "@mui/material";
 import  FooterItems  from "./FooterItems";
+import styled from 'styled-components';
 export default function Footer () {
+    const FooterContainer = styled.div`
+    padding: 20px;
+    background: #CED0CE;
+    `;
+
     return (
-        <footer>
-        <Box sx={{
-            backgroundColor: "gray"
-        }}>
+        <FooterContainer>
+        <Box>
             <Container maxWidth="lg">
-                <Grid container spacing={5}>
-                    <Grid item xs={12} sm={4}>
-                        <FooterItems/>
-                    </Grid>
+                <Grid container direction="row" spacing={5}>
+                    <FooterItems/>
                 </Grid>
+                <Box>
+                    <img src="..src/SocialIcons/twitterlogo.svg"
+                    alt="Twitter logo"/>
+                </Box>
+                <Box>
+                    House Sparrow Practicum &reg; {new Date().getFullYear()}
+                </Box>
             </Container>
         </Box>
-        </footer>
+        </FooterContainer>
     )
 }
