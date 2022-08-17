ec0f8ca6fe0cddea2b9ea200230d3c0c9ed14351
Irene Sanchez <irene.iscc777@gmail.com>
2022-08-17 14:19:30 -0700

    adds home page buttons and donate button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,40 @@
 import React from "react"
+import { Button, Stack, Container } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>
+  // the useNavigate hook navigates to the other routers
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Container>
+        <h1>Every Pet Deserves A Home</h1>
+        <Stack
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          spacing={8}>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={() => navigate("/adopt")}
+          >
+            Adopt Now
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={() => navigate("/volunteer")}
+          >
+            Volunteer
+          </Button>
+        </Stack>
+      </Container>
+    </div>
+  )
 }
 
 export default Home
