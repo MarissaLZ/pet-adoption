bba31c850db1a86480fb91c85edeb30a406b1964
Irene Sanchez <irene.iscc777@gmail.com>
2022-08-17 16:50:21 -0700

    Changed header, added a small blurb, and shifted to the left

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,46 @@
 import React from "react"
-import { Button, Stack, Container } from "@mui/material";
+import { Button, Stack, Grid, Box } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  // the useNavigate hook navigates to the other routers
+  // the useNavigate hook navigates to the other routes
   const navigate = useNavigate();
 
   return (
     <div>
-      <Container>
-        <h1>Every Pet Deserves A Home</h1>
-        <Stack
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-          spacing={8}>
-          <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            onClick={() => navigate("/adopt")}
-          >
-            Adopt Now
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            size="large"
-            onClick={() => navigate("/volunteer")}
-          >
-            Volunteer
-          </Button>
-        </Stack>
-      </Container>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <h1>Every Pet Deserves A Home</h1>
+            <Box m={2} sx={{ p: 2 }}>
+              <p>Join our mission to rehome the most vulnerable animals and enhance the lives of pets and people</p>
+              <p>Every animal is important to us - help us save lives.</p>
+            </Box>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={8}>
+              <Button
+                variant="contained"
+                color="secondary"
+                size="large"
+                onClick={() => navigate("/adopt")}
+              >
+                Adopt Now
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                size="large"
+                onClick={() => navigate("/volunteer")}
+              >
+                Volunteer
+              </Button>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   )
 }
