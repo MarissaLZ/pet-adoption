1c49581458098967ce8a8508ed508977e18c440c
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-08-23 14:26:48 -0400

    fixes conflicts in routing

--- src/components/fetchPetList.js
+++ src/components/fetchPetList.js
@@ -1,23 +1,23 @@
 //fetchPetList is a function that accepts userZipCode to get a list of 20 animals based on the users location
-function fetchPetList (userZipCode) {
-  return (
-    fetch("https://api.petfinder.com/v2/oauth2/token", {
-      body: `grant_type=client_credentials&client_id=${process.env.REACT_APP_PETFINDER_API_KEY}&client_secret=${process.env.REACT_APP_PETFINDER_CLIENT_SECRET}`,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      method: "POST"
-    })
+function fetchPetList(zipcode, animalType) {
+  return fetch("https://api.petfinder.com/v2/oauth2/token", {
+    body: `grant_type=client_credentials&client_id=${process.env.REACT_APP_PETFINDER_API_KEY}&client_secret=${process.env.REACT_APP_PETFINDER_CLIENT_SECRET}`,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "POST",
+  })
     .then((response) => response.json())
     .then((result) =>
-      fetch(`https://api.petfinder.com/v2/animals?type=dog&location=${userZipCode}`, {
-        headers: {
-          Authorization: `Bearer ${result.access_token}`
+      fetch(
+        `https://api.petfinder.com/v2/animals?type=${animalType}&location=${zipcode}`,
+        {
+          headers: {
+            Authorization: `Bearer ${result.access_token}`,
+          },
         }
-      })
-      .then((response) => response.json())
+      ).then((response) => response.json())
     )
-  )
 }
 
-export default fetchPetList;
+export default fetchPetList
