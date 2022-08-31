45bafa7483c37f79c0dbd9aa02e3b5e408f0e959
Monica Delgado <mnca.delgado@gmail.com>
2022-08-31 02:03:01 -1100

    restrutures firestore fetch functions
    Co-authored-by: MarissaLZ <MarissaLZ@users.noreply.github.com>

--- src/Firebase/FirebaseFirestoreServices.js
+++ src/Firebase/FirebaseFirestoreServices.js
@@ -1,19 +1,28 @@
 import firebase from "./FirebaseConfig"
 
 //Adds a favorite
-export const handleAddLike = async (petFinderId) => {
-  await firebase.firestore().collection("likes").add({
-    petFirebaseId: petFinderId,
+export const handleAddLike = async (pet) => {
+  console.log("handleAdd Like Pet", pet.id)
+  //must use pet id as a string and NOT a number
+  await firebase.firestore().collection("likes").doc(pet.id.toString()).set({
+    id: pet.id,
+    breeds: pet.breeds,
+    age: pet.age,
+    name: pet.name,
+    distance: pet.distance,
+    photos: pet.photos,
+    //pet,
   })
 }
 
 //Deletes a favorite
-export const handleDelete = async (petFinderId) => {
+export const handleDelete = async (pet) => {
+  console.log("Handle delete")
   const snapshot = await firebase
     .firestore()
     .collection("likes")
     .limit(1)
-    .where("petFirebaseId", "==", petFinderId)
+    .where("id", "==", pet.id)
     .get()
 
   const doc = snapshot.docs[0]
