daa5376dabfa57a94ce66b6e852ce72600bdcf05
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-08-30 21:31:30 -0400

    signs out user and redirects to login
    Co-authored-by: Monica <mnca16@users.noreply.github.com>

---
+++ src/components/Logout.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { Button } from "@mui/material"
+import firebase from "../Firebase/FirebaseConfig"
+import { useNavigate } from "react-router-dom"
+
+import { useContext } from "react"
+import { UserContext } from "../context"
+
+const Logout = () => {
+  const { isLoggedIn, setIsLoggedIn } = useContext(UserContext)
+  //hook
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setIsLoggedIn(!isLoggedIn)
+        navigate("login")
+      })
+      .catch((error) => {
+        console.log("Could not log out", error)
+      })
+  }
+  return (
+    <Button color="inherit" onClick={handleLogout}>
+      Logout
+    </Button>
+  )
+}
+
+export default Logout
