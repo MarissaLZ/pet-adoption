0ece6c27f4e617843094034170368fb14dc8e49e
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-09-04 21:10:20 -0400

    creates fetch request with display limit on pets

--- src/components/petFinderAPI.js
+++ src/components/petFinderAPI.js
@@ -41,3 +41,21 @@ export function fetchPet(id) {
       }).then((response) => response.json())
     )
 }
+
+export function fetchFeatured(limitNum) {
+  return fetch("https://api.petfinder.com/v2/oauth2/token", {
+    body: `grant_type=client_credentials&client_id=${process.env.REACT_APP_PETFINDER_API_KEY}&client_secret=${process.env.REACT_APP_PETFINDER_CLIENT_SECRET}`,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((result) =>
+      fetch(`https://api.petfinder.com/v2/animals?limit=${limitNum}`, {
+        headers: {
+          Authorization: `Bearer ${result.access_token}`,
+        },
+      }).then((response) => response.json())
+    )
+}
