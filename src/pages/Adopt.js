61dd72012663bc03773991b379a3c974c12db8c7
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-09-04 21:17:10 -0400

    renders a featured pets component on the first load

--- src/pages/Adopt.js
+++ src/pages/Adopt.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import PetList from "../components/PetList"
 import SortDropDown from "../components/SortDropDown"
 import { LoadingContext } from "../context"
@@ -9,11 +9,21 @@ import AdoptPagination from "../components/AdoptPagination"
 import { Box } from "@mui/material"
 import { useContext } from "react"
 import { PetsContext, SearchContext } from "../context"
-import AdoptInfo from "../components/AdoptInfo"
+import FeaturedPets from "../components/FeaturedPets"
+import { fetchFeatured } from "../components/petFinderAPI"
 
 const Adopt = () => {
   const { petList } = useContext(PetsContext)
   const { search } = useContext(SearchContext)
+  const { setFeaturedPets } = useContext(PetsContext)
+
+  //fetch featured pets only on the first render of the adopt page
+  useEffect(() => {
+    //set state of featured pets
+    fetchFeatured("3").then((response) => {
+      setFeaturedPets([...response.animals])
+    })
+  }, [])
 
   const [isLoading, setIsLoading] = useState(false)
   return (
@@ -29,9 +39,13 @@ const Adopt = () => {
           <Search />
 
           {search.validSearch ? (
-            <PetList animalList={petList} />
+            <>
+              <SortDropDown />
+              <PetList animalList={petList} />
+              <AdoptPagination />
+            </>
           ) : (
-            <AdoptInfo />
+            <FeaturedPets />
           )}
 
           {/* {petList ? (
