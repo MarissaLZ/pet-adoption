3cae5a22b7d930d1477f889e64602c284cf02f3c
Jay <95107672+Jay-zar@users.noreply.github.com>
2022-09-13 15:34:38 -0400

    Created a test vertion of the filtering printed as console logs.

--- src/components/FilterContainer.js
+++ src/components/FilterContainer.js
@@ -6,9 +6,13 @@ const FilterContainer = () => {
         filterGenderOption, setFilterGenderOption, 
         filterSizeOption, setFilterSizeOption,
         filterKidsOption, setFilterKidsOption} = useContext(FurrdoptionContext)
-    const genderSelectionValues = ["Female", "Male"]
-    const sizeSelectionValues = ["Small","Large"]
-    const kidsSelectionValues = ["Good with Kids", "Not good with Kids"]
+    const genderSelectionValues = ["Female", "Male", "Unknown"]
+    const sizeSelectionValues = ["Small", "Medium", "Large"]
+    const kidsSelectionValues = ["Good with Kids"]
+    //petList[i].environment.Good_with_kids is not a string the matches the menu text, 
+    //so it needs seperate terms for the filtering
+    const kidsFilterTerms = [true]
+
     return(
         /*
         The filter boxes that each have the following set of props:
@@ -21,18 +25,21 @@ const FilterContainer = () => {
                 selectionValue={genderSelectionValues}
                 filterState={filterGenderOption}
                 filterSetter={setFilterGenderOption}
+                filterParams={genderSelectionValues}
             />
             <FilterDropdown 
                 filterType={"Filter by size"} 
                 selectionValue={sizeSelectionValues}
                 filterState={filterSizeOption}
                 filterSetter={setFilterSizeOption} 
+                filterParams={sizeSelectionValues}
             />
             <FilterDropdown 
                 filterType={"Status with Kids"} 
                 selectionValue={kidsSelectionValues}
                 filterState={filterKidsOption}
                 filterSetter={setFilterKidsOption} 
+                filterParams={kidsFilterTerms}
             />
         </>
     )
--- src/components/FilterDropdown.js
+++ src/components/FilterDropdown.js
@@ -4,19 +4,47 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { FurrdoptionContext } from '../FurrdoptionProvider';
-
+let filteredPetList = []
 //I need to have the selection set the petList from the filter. Import the context, figure
 //out the labels
-const FilterDropdown = ({selectionValue, filterType, filterState, filterSetter}) => {
-   const {
+//
+//Save petList to new state of filteredList-figure out how to display filtered list if filtered, 
+//else revert to petList
+//
+//Maybe store filter params as state, have map or forEach use of custom filter function?
+//
+const FilterDropdown = ({selectionValue, filterType, filterState, filterSetter, filterParams}) => {
+   const {petList, setPetList,
     filterGenderOption, setFilterGenderOption, 
     filterSizeOption, setFilterSizeOption,
     filterKidsOption, setFilterKidsOption} = useContext(FurrdoptionContext)
-  
-  const handleChange = (e: SelectChangeEvent) => {
-    filterSetter(e.target.value)
-    console.log("event", e.target.value)
-  };
+  const handleFilter = (filterParams) => {
+    for (let i = 0 ; i < petList.length ; i++ ) 
+      if(petList[i].filterParams = filterParams) {
+        filteredPetList.push(petList[i])
+      }
+    }
+
+  // }
+  //fix the filter and return below!!!!!!!
+  const handleChange = (e) => {
+    //console.log("event", e.target.value)
+    if (e.target.value != filterType) {
+      filterSetter(e.target.value)
+      handleFilter(filterParams)
+    }
+    else {
+      console.log("Not a filter term")
+    }
+    console.log(filteredPetList)
+    // setIsLoading(true)
+    // petList.filter((item) => {
+    //     return 
+    // })
+    // //setPetList()
+    // setPageCount(response.pagination.total_pages)
+    // setIsLoading(false)
+  }
 
   return (
     <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
@@ -32,8 +60,9 @@ const FilterDropdown = ({selectionValue, filterType, filterState, filterSetter})
         <MenuItem value={filterType}>
             <em>{filterType}</em>
         </MenuItem>
-        <MenuItem value={selectionValue[0]}>{selectionValue[0]}</MenuItem>
-        <MenuItem value={selectionValue[1]}>{selectionValue[1]}</MenuItem>
+        {selectionValue.map((option) => (
+          <MenuItem key={option} value={option}>{option}</MenuItem>
+        ))}
       </Select>
     </FormControl>
   );
