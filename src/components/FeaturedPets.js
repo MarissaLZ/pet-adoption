c50bd4ab7a7b14fa2e0b13ad994b2801d98633b9
Marissa Lazo <marissalazo@Marissas-MacBook-Pro.local>
2022-09-13 00:38:43 -0400

    makes featured pets responsive and centered

--- src/components/FeaturedPets.js
+++ src/components/FeaturedPets.js
@@ -7,31 +7,29 @@ const FeaturedPets = () => {
   const { featuredPets } = useContext(FurrdoptionContext)
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh",
-        width: {
-          xs: "400px",
-          sm: "600px",
-          md: "800px",
-          lg: "850px",
-          xl: "1200px",
-        },
-        margin: "0 auto",
-        color: "#606060",
-        gap: "3rem",
-      }}
-    >
-      <h1>
-        Feature <span style={{ color: "#26A69A" }}>Pets</span>
-        <br />
-        <h6>Adorable adoptables near you!</h6>
-      </h1>
-
+    <>
+      <Box
+        sx={{
+          width: {
+            xs: "400px",
+            sm: "600px",
+            md: "800px",
+            lg: "900px",
+            xl: "1200px",
+          },
+          margin: "0 auto",
+          color: "#606060",
+          gap: "1rem",
+        }}
+      >
+        <h1>
+          Featured <span style={{ color: "#26A69A" }}>Pets</span>
+          <br />
+          <h6>Adorable adoptables near you!</h6>
+        </h1>
+      </Box>
       <PetList animalList={featuredPets} />
-    </Box>
+    </>
   )
 }
 
