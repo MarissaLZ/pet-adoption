f3478416df83c2bcc3ddbb69822ed05ce2670249
Monica Delgado <mnca.delgado@gmail.com>
2022-09-19 13:33:25 -1100

    deleted some comments

--- src/App.js
+++ src/App.js
@@ -26,11 +26,8 @@ function App() {
         <Route path="/petbio/:id" element={<PetBio />} />
         <Route path="Signup" element={<Signup />} />
         <Route path="favorites" element={<Favorites />} />
-<<<<<<< HEAD
         <Route path="Profile" element={<Profile />} />
-=======
         <Route path="/Home" element={<Home />} />
->>>>>>> main
       </Routes>
       <Footer />
     </div>
